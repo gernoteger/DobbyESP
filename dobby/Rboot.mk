be48c085ac4ff07cc8654743ab3f7298fd4e58f0
Gernot Eger <gernot.eger@gmail.com>
2016-01-24 17:50:21 +0100

    build cleanup: startwith rboot

---
+++ dobby/Rboot.mk
@@ -0,0 +1,10 @@
+#####################################################################
+#### call the rboot makefiles.. 										####
+#####################################################################
+# Including user Makefile.
+# Should be used to set project-specific parameters
+include ./Makefile-user.mk
+
+
+include ../raburton_esp8266/rboot/Makefile
+
